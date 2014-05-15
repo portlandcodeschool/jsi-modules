85b4bc494a62a5c59363d3423d47b719fc0e15ad
Dmitry White <white.dmitry@gmail.com>
2014-05-15 13:13:29 -0700

    Ignored node_modules files

--- test/tests.js
+++ test/tests.js
@@ -11,17 +11,6 @@ var pluralize = function(wurd) {
 }
 
 describe('_.map()', function() {
-	it('fails a test', function() {
-		expect(_.map(1)).to.eql(0);
-	});
-
-	it('returns the same array I send it', function () {
-		expect(_.map([1,2], twoTimes)).to.eql([1,2])
-	});
-
-	it('performs a function that I pass to it', function () {
-		expect(_.map(1, twoTimes)).to.eql(2);
-	});
 
 	it('converts an array of [1, 2] to [2, 4] ' +
 		'when provided the function {*2}', function () {
@@ -30,5 +19,10 @@ describe('_.map()', function() {
 
 	);
 
-	it
+	it('converts ["pod", "pond", "rattle", "snake"] to ' +
+		'["pods", "ponds", "rattles", "snakes"] when provided ' +
+		'the function {add an s}', function() {
+			expect(_.map(["pod", "pond", "rattle", "snake"], pluralize)).to.
+			eql(["pods", "ponds", "rattles", "snakes"]);
+		})
 })
